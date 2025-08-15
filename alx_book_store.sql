010417b3b3f66e241e37506567c5262f68b5ce5b
Darlene-13 <darlenenasimiyu@gmail.com>
2025-08-16 00:39:12 +0300

    Updated order details

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -36,7 +36,7 @@ CREATE TABLE Customers (
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
-    order_date DATE DEFAULT CURRENT_DATE,
+    order_date DATE DEFAULT (CURRENT_DATE),
     total_amount DECIMAL(10, 2) NOT NULL,
     status ENUM('pending', 'completed', 'cancelled') DEFAULT 'pending',
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
@@ -46,7 +46,7 @@ CREATE TABLE Order_details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     book_id INT NOT NULL,
-    quantity INT NOT NULL,
+    quantity DOUBLE NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
