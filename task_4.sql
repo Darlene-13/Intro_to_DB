0b4d0b28f24d035d05d936a757d870df37d3a487
Darlene-13 <darlenenasimiyu@gmail.com>
2025-08-16 09:05:46 +0300

    Full books table desription

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,14 @@
 # Show full description of the table books
-DESCRIBE Books;
-# Show full description of the table authors
+SELECT
+    COLUMN_NAME,
+    DATA_TYPE,
+    COLUMN_DEFAULT,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    EXTRA,
+    COLUMN_TYPE
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
