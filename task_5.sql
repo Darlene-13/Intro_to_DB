be405a4b22265fc5acf380f73ddf3b9d141378ec
Darlene-13 <darlenenasimiyu@gmail.com>
2025-08-16 09:09:25 +0300

    Insert a row to the customers table

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+---- INSERT A SINGLE ROW to customers table
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Avenue');  
