a948ace3d68f300e2e7132aea4750092affc6915
Darlene-13 <darlenenasimiyu@gmail.com>
2025-08-16 09:11:23 +0300

    Address update

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
 ---- INSERT A SINGLE ROW to customers table
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Avenue');
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
